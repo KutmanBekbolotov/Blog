75d43c63a235a4a88da8a31563e9a84350665e95
kutman <kutmanbekbolotov2003@gmail.com>
2025-01-23 14:33:31 +0600

    fixed user table

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Blog } from '../blog/blog.entity'; 
 
-@Entity()
+@Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
